9b747b523e566ff1de9c443596467addb382b22a
Artemii Oliinyk <aoliinyk@azercell.org>
2023-05-08 06:48:30 -0400

    Remove unused files

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <edit_map/edit_map_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  EditMapPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("EditMapPluginCApi"));
 }
